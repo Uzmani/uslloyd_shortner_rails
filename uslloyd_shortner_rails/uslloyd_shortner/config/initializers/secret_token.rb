fb5e2ef6da61484de6a3129a9772993441921569
Dev Bootcamp <Apprentice@dbc35.local>
2013-09-16 18:33:59 -0700

    initial rails app

---
+++ uslloyd_shortner_rails/uslloyd_shortner/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UslloydShortner::Application.config.secret_token = '3965a8cc2ea25d441ba7d95a1886a2bda9636ba1f6af6ddd32ab4b26d13eb7450dd05239afdef07694cd97d087bd47514f82e433dc93663a340de2fbe4c19a99'
